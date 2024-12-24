3e26bf7782a95d63a9ec4ca577ae1ffb286ba205
micropuma <micropuma@163.com>
2024-12-24 17:41:52 +0800

    add some files

---
+++ tutorial/GEMM/09_gemm_extern_vec_4x4/generate-elf.sh
@@ -0,0 +1,3 @@
+python aie.py
+
+python ../../../aiecc.py --xchesscc --xbridge --no-aiesim --aie-generate-cdo --aie-generate-npu --no-compile-host --xclbin-name=aie.xclbin --npu-insts-name=insts.txt aie.mlir
