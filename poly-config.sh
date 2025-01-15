5364cfaa7291de88a571185abbb9cc576d3f8dac
micropuma <micropuma@163.com>
2025-01-15 23:02:49 +0800

    E2E script

---
+++ poly-config.sh
@@ -0,0 +1,2 @@
+export CUDACXX=/usr/local/cuda/bin/nvcc
+export PATH=$PATH:/home/douliyang/large/mlir-workspace/Polygeist/build/bin
---
+++ tutorial/E2E/matmul/one_dim/phase1.sh
@@ -0,0 +1,15 @@
+air-opt input2.mlir \
+  --air-insert-launch-and-segment-around-herd \
+  --air-lower-herd-parallel \
+  --canonicalize \
+  --cse \
+  --air-specialize-channel-wrap-and-stride \
+  --air-renumber-dma \
+  --convert-linalg-to-loops \
+  --mlir-print-ir-after-all \
+  -o output1.mlir \
+  2>&1 | tee output.dump
+
+air-opt output1.mlir \
+  --air-place-herds{num-rows=6 num-cols=10 row-anchor=2 col-anchor=7}
+  -o output2.mlir
